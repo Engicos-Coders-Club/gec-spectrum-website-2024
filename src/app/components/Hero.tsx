f47e1444f494e5b774199023cfb9a2ae8efa117c
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2024-03-05 16:08:19 +0530

    added Spectrum title card

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import localFont from 'next/font/local'
 import { Space_Grotesk } from 'next/font/google'
 
@@ -9,7 +8,7 @@ const space = Space_Grotesk({
   })
 
 // Font files can be colocated inside of `app`
-const myFont = localFont({
+const panchang = localFont({
     src: '../../../public/Panchang-Variable.ttf',
     display: 'swap',
   })
@@ -18,9 +17,9 @@ const Hero = () => {
   return (
     <div className='h-fit w-fit flex flex-col items-center justify-center' style={{backgroundImage:'url("/Vector.svg")'}}>
         <div className='md:h-[80vh] h-[60vh] w-screen flex flex-col items-center justify-center text-center'>
-            <h1 className={`${myFont.className} md:text-9xl text-4xl font-bold line-clamp-1`}>SPECTRUM</h1>
+            <h1 className={`${panchang.className} md:text-9xl text-4xl font-bold line-clamp-1`}>SPECTRUM</h1>
             <div className='bg-gradient-to-b from-white to-[#111111] text-transparent bg-clip-text'>
-                <h2 className={`md:text-4xl font-bold text-xl ${myFont.className}`}>2024</h2>
+                <h2 className={`md:text-4xl font-bold text-xl ${panchang.className}`}>2024</h2>
             </div>
         </div>
         <div className='bg-[#FFBA25] w-11/12 md:h-10 rounded-xl rounded-tl-none flex items-center md:justify-between text-black  md:flex-row flex-col justify-center'>
@@ -31,7 +30,6 @@ const Hero = () => {
                 <h3 className={`${space.className} md:mr-16`}>/ * GOA COLLEGE OF ENGINEERING */</h3>
             </div>
         </div>
-        <div className='md:w-1/3 text-center md:text-2xl font-medium mt-40 tracking-widest'><p className={space.className}>“EMBRACE THE UNKNOWN & GLIDE WITH US THROUGH THE COSMIC SPECTRUM”</p></div>
     </div>
   )
 }
