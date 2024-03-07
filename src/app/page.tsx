273f8cf59ece081eca51627be19cc173dca20a25
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2024-03-07 18:31:42 +0530

    Sponsors component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 import Hero from "./components/Hero";
 import EventCard from "./components/Events";
 import Expo from "./components/Expo";
+import Sponsors from "./components/Sponsors";
 
 export default function Home() {
   return (
-    <>
-    <section className="overflow-hidden">
-    <Hero/>
-
-    </section>
-      <section className="overflow-hidden mt-20 p-2">
+    <div className="overflow-hidden">
+        <Hero/>
         <EventCard/>
-      </section>
-      <section className="overflow-hidden">
-      <Expo/>
-      </section>
-     
-    </>
+        <Expo/>
+        <Sponsors/>
+    </div>
   );
 }
