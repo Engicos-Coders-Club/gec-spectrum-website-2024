639a695e2d73910a6ffe2fbfa1d8ceef99e912a1
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2024-03-09 19:41:28 +0530

    Update Tagline.tsx

--- src/app/components/Tagline.tsx
+++ src/app/components/Tagline.tsx
@@ -3,10 +3,10 @@ import { url } from "inspector";
 import { Space_Grotesk } from "next/font/google";
 
 const space = Space_Grotesk({
-    weight: ['700'],
-    subsets: ['latin'],
-    display: 'swap',
-  })
+  weight: ["700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const Tagline = () => {
   return (
@@ -14,12 +14,15 @@ const Tagline = () => {
       <div
         className="h-[10vh] w-screen flex flex-col items-center justify-center"
         style={{ backgroundImage: 'url("/Vector.svg")' }}
-      >
-      </div>
+      ></div>
 
-      <div className={`${space.className} text-2xl text-center`} >
-          “EMBRACE THE UNKNOWN & GLIDE <br /> WITH US THROUGH  THE COSMIC <br /> SPECTRUM”
-        </div>
+      <div className={`${space.className} text-3xl text-center relative`}>
+        <div className="h-[0.1rem] gradient-five-colors w-full absolute top-1/2 -z-10"></div>
+        <p className="z-10">
+          “EMBRACE THE UNKNOWN & GLIDE <br /> WITH US THROUGH THE COSMIC <br />{" "}
+          SPECTRUM”
+        </p>
+      </div>
     </div>
   );
 };
