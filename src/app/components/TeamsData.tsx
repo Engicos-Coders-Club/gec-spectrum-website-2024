6f2ddab4b19ebee5d08aee6c9fb9d046704c10dd
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2024-03-12 18:23:27 +0530

    added animations

--- src/app/components/TeamsData.tsx
+++ src/app/components/TeamsData.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { Teamdata } from "@/assets/Teamdata";
 import { Space_Grotesk } from 'next/font/google'
+import { motion } from "framer-motion";
 
 const space = Space_Grotesk({
     weight: ['700','500','300'],
@@ -18,7 +19,7 @@ const TeamsData = () => {
   return (
     <div>
       <div className="w-full flex justify-center mt-10 p-3 overflow-hidden">
-        <div className="grid grid-rows-2 p-5 grid-cols-3 sticky top-0 gap-10  ${Space.className} lg:grid-cols-9 lg:gap-4">
+        <div className="grid grid-rows-2 p-5 grid-cols-3 sticky top-0 gap-10  ${Space.className} lg:grid-cols-9 lg:gap-4 lg:grid-rows-1">
           {Teamdata.map((button, index) => {
             return (
               <div
@@ -48,17 +49,23 @@ const TeamsData = () => {
           <div className="gap-7 grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 w-screen h-auto p-10 grid-cols-2">
             {Teamdata[selectedDepartment].people?.map((person, index) => {
               return (
-                <div key={index} className="relative border border-white">
+                <motion.div className="relative border border-white hover:border-[#FFBA25]"
+                key={`${selectedDepartment}-${index}`}
+                initial={{y:50,opacity:0}}
+                animate={{y:0,opacity:1}}
+                transition={{duration:1,delay:0.3}}
+
+                >
                   <img
                     src={person.image}
                     alt="profile picture"
-                    className="w-full h-full object-cover"
+                    className="w-full h-full object-cover grayscale"
                   />
                   <div className="absolute bottom-0 left-0 right-0 bg-black p-3">
                     <h2 className="text-[#FFBA25] md:text-2xl text-xl">{person.name}</h2>
                     <h3 className="text-white font-light sm:text-base text-sm">{person.designation}</h3>
                   </div>
-                </div>
+                </motion.div>
               );
             })}
           </div>
