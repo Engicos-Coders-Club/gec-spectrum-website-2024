aedc46887e815c03ab3f288ae8cee0e1b616fb05
Harsh Telang <telangharsh06@gmail.com>
2024-03-12 19:10:07 +0530

    Base styling complete

---
+++ src/utils/data.example.ts
@@ -0,0 +1,19 @@
+export const EVENT = [
+    {
+        title: "ROBO SOCCER",
+        subtitle: "ROBO SOCCER",
+        date: "3 MARCH 2023",
+        type: "Team",
+        team_size: "3 - 5",
+        contacts: [
+            {
+                name: "Harsh Kumar",
+                contact: "/"
+            },
+            {
+                name: "Shriteen Damaskar",
+                contact: "/"
+            }
+        ]
+    }
+]
