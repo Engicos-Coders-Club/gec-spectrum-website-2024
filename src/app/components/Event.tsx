58ba0261bc4377c1a4a1e682a1640749461a813c
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2024-03-13 18:56:39 +0530

    fixed event component bug

--- src/app/components/Event.tsx
+++ src/app/components/Event.tsx
@@ -17,7 +17,7 @@ const space = Space_Grotesk({
 
 const Event = () => {
   return (
-    <div className={`${space.className} w-screen mt-96 relative sm:h-[150vh] h-[250vh]  sm:mb-80 lg:mb-5`}>
+    <div className={`${space.className} w-screen mt-96 relative sm:h-[150vh] h-[300vh]  sm:mb-80 lg:mb-5`}>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-50"
       >
         <div className="absolute w-full h-full bg-gradient-to-br from-[#6B46C1] via-[#5933A6] to-[#4C1D95] rounded-[6rem] filter blur-3xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-50"
