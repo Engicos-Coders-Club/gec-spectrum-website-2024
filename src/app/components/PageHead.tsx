068b87272e0220773160c1c58642453d1c944c74
Introvert2000 <alvareschamp200@gmail.com>
2024-03-16 16:06:35 +0530

    competiton text was not proper in mobile view

--- src/app/components/PageHead.tsx
+++ src/app/components/PageHead.tsx
@@ -22,7 +22,7 @@ const PageHead = ({body, title, faltutext}:PageHeadProps) => {
     <div className={`${panchang.className} w-screen md:h-56  h-40 flex border border-b-[#FFBA25] border-t-white border-x-0`}>
      <div className="w-4/6 h-full sm:p-16 p-10 sm:border-r border-[#FFBA25] flex items-start justify-center flex-col ">
         <h2 className="text-[#741CFF] font-semibold sm:text-2xl text-lg">{body}</h2>
-        <h1 className='font-semibold sm:text-6xl text-5xl'>{title}</h1>
+        <h1 className='font-semibold sm:text-5xl text-2xl'>{title}</h1>
      </div>
      <div className="w-2/6 h-full sm:flex justify-center items-center hidden">
         <p className={`${spacemono.className} md:text-[10rem]  text-[#741CFF] text-7xl`}> {faltutext}</p>
