3b1628e486ae47e84bd258d12163a72fa563cafe
Rushikesh <141911720+Rushikesh-24@users.noreply.github.com>
2024-03-17 13:30:01 +0530

    Api integrated in competition page (#54)
    * competitions page api integrated
    
    * Style fixes
    
    ---------
    
    Co-authored-by: shrey <shreysingh152004@gmail.com>
    Co-authored-by: Shridhar Kamat <52820662+Shridhar-dev@users.noreply.github.com>
    Co-authored-by: Greeshma Medam <greeshma.medam@gmail.com>

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,97 +1,99 @@
-"use client";
+"use client"
 
-import Image from "next/image";
-import Logo from "@/assets/spectrum-logo.png";
-import { useState } from "react";
-import Link from "next/link";
-import FullWidth from "./FullWidth";
-import { motion } from "framer-motion";
+import Image from "next/image"
+import Logo from "@/assets/logo.png"
+import { useState } from "react"
+import Link from "next/link"
+
+import FullWidth from "./FullWidth"
+// import LoginModal from "@/app/components/LoginModal"
+import { motion } from "framer-motion"
 function Navbar() {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    }
+    const handleOpenModal = (event:any) => {
+        event.preventDefault();
+        setIsModalOpen(true);
+    }
 
   const links = [
     {
-      name: "COMPETITIONS",
-      to: "/competitions",
+        name:"COMPETITIONS",
+        to: "/competitions"
     },
     {
       name: "EXPO",
-      to: "#expo",
+      to: "/#expo",
     },
     // {
     //     name:"SPONSORS",
     //     to: "#sponsors"
     // },
     {
-      name: "CONTACT",
-      to: "/#contact",
-    },
-  ];
+        name:"CONTACT",
+        to: "#contact"
+    }
+   
+  ]
   return (
-    <motion.nav
-      className="px-10 sm:px-16 py-5 flex justify-between items-center sticky top-0 z-50 w-full backdrop-blur"
-      initial={{ y: -100 }}
-      animate={{ y: 0 }}
-      transition={{ duration: 1 }}
+    <motion.nav className="bg-[#06050a] px-10 sm:px-16 py-5 flex justify-between items-center"
+    initial={{y:-100}}
+    animate={{y:0}}
+    transition={{duration:1}}
     >
-      <div className="flex items-center justify-between flex-1 lg:flex-auto lg:justify-start gap-5">
-        {!open ? (
-          <button className="cursor-pointer">
-            <svg
-              onClick={() => setOpen((prev) => !prev)}
-              className="order-2 lg:order-1 mb-2"
-              width="45"
-              height="18"
-              viewBox="0 0 45 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <line y1="1" x2="45" y2="1" stroke="white" />
-              <line y1="9" x2="45" y2="9" stroke="white" />
-              <line y1="17" x2="45" y2="17" stroke="white" />
-            </svg>
-          </button>
-        ) : (
-          <FullWidth close={setOpen} />
-        )}
-        <div className="order-1 lg:order-2W">
-          <Link href={"/"} className="cursor-pointer">
-            <Image
-              src={Logo.src}
-              width={Logo.width-900}
-              height={Logo.height-900}
-              className="object-contain cursor-pointer"
-              alt="logo"
-            />
-          </Link>
-        </div>
-      </div>
-      <div className="items-center gap-5 hidden lg:flex">
-        {links.map((link, i) => (
-          <Link
-            href={link.to}
-            key={i}
-            className=" border-light-50 relative flex justify-center rounded-full"
-          >
-            <div
-              className="m-[1px] p-2 px-5 border border-dashed border-transparent hover:border-dotted hover:border-indigo-50"
-              style={{ borderRadius: "0px 20px 20px 20px" }}
-            >
-              {link.name}
+        <div className="flex items-center justify-between flex-1 lg:flex-auto lg:justify-start gap-5">
+            {
+                !open ?
+                <svg onClick={()=>setOpen((prev)=>!prev)} className="order-2 lg:order-1 mb-2" width="45" height="18" viewBox="0 0 45 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line y1="1" x2="45" y2="1" stroke="white"/>
+                    <line y1="9" x2="45" y2="9" stroke="white"/>
+                    <line y1="17" x2="45" y2="17" stroke="white"/>
+                </svg> :
+                <FullWidth close={setOpen}/>
+            }
+            <div className="order-1 lg:order-2W">
+                <Link href={'/'}>
+                <Image 
+                    src={Logo.src}
+                    width={Logo.width}
+                    height={Logo.height}
+                    className="cursor-pointer"
+                    alt="logo"
+                />
+                </Link>
+                
             </div>
-          </Link>
-        ))}
-
-        {/* <button className="bg-primary p-2 px-5 rounded-full rounded-tl-none">
+        </div>
+        <div className="items-center gap-5 hidden lg:flex">
+        {/* <div>
+      <h1  onClick={handleOpenModal} className="cursor-pointer">
+        LOGIN
+      </h1>
+      {isModalOpen && <LoginModal onClose={handleCloseModal} />}
+    </div> */}
+            {
+                links.map((link, i)=>(
+                    <Link href={link.to} key={i} className=" hover:bg-dotted-border bg-[#06050a] relative flex justify-center rounded-full overflow-hidden">
+                        <div className="bg-[#06050a] m-[1px] p-2 px-5 outline-dotted rounded-full outline-2 outline-black">
+                            {link.name}
+                        </div>
+                    </Link>
+                ))
+            }
+            
+            {/* <button className="bg-primary p-2 px-5 rounded-full rounded-tl-none">
                 BROCHURE
             </button> */}
 
-        {/* <button className="bg-yellow-500 p-2 px-5 rounded-full rounded-tl-none text-black">
+            {/* <button className="bg-yellow-500 p-2 px-5 rounded-full rounded-tl-none text-black">
                 SIGN IN
             </button> */}
-      </div>
+        </div>
     </motion.nav>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
