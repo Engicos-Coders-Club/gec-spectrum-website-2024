31d5ea0aa71c40506407a650de282a2e9f016441
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2024-03-17 18:20:02 +0530

    Update page.tsx

--- src/app/competitions/[slug]/page.tsx
+++ src/app/competitions/[slug]/page.tsx
@@ -7,7 +7,7 @@ import { IoChevronBack } from "react-icons/io5";
 import { RiInstagramFill, RiLinkM } from "react-icons/ri";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { RWebShare } from "react-web-share"
+import { RWebShare } from "react-web-share";
 
 interface EventData {
   event: {
@@ -17,6 +17,10 @@ interface EventData {
         contact: string;
         _id: string;
       }[];
+    };
+    teamSize: {
+      min: number;
+      max: number;
     };
     rulesAndRegulations: {
       topic: string;
@@ -31,7 +35,6 @@ interface EventData {
       secondPlace: number;
     };
     date: string;
-    teamSize: number;
   };
 }
 
@@ -46,30 +49,33 @@ const panchang = localFont({
   display: "swap",
 });
 
-
 const page = ({ params }: { params: { slug: string } }) => {
   const [events, setEvents] = useState<EventData>({} as EventData);
 
   useEffect(() => {
-    (async()=>{
-      const response = await axios.get(`https://gec-spectrum-backend-2024.2.sg-1.fl0.io/api/v1/events/${params.slug}`)
+    (async () => {
+      const response = await axios.get(
+        `https://gec-spectrum-backend-2024.2.sg-1.fl0.io/api/v1/events/${params.slug}`
+      );
       const data = await response.data;
       setEvents(data);
-    })()
-  }, [params.slug])
+    })();
+  }, [params.slug]);
 
   const handleSubmit = () => {
     console.log("Button is working");
-    const RegistrationLink = events?.event?.rulesAndRegulations?.find(rule => rule.topic === "Registration Link:")?.points[0];
-    
+    const RegistrationLink = events?.event?.rulesAndRegulations?.find(
+      (rule) => rule.topic === "Registration Link:"
+    )?.points[0];
+
     if (RegistrationLink) {
-        console.log('Registration Link:', RegistrationLink);
-        window.location.href = RegistrationLink;
+      console.log("Registration Link:", RegistrationLink);
+      window.location.href = RegistrationLink;
     } else {
-        console.error('Registration link not found or invalid.');
-        // Optionally, you can show an error message to the user here.
+      console.error("Registration link not found or invalid.");
+      // Optionally, you can show an error message to the user here.
     }
-};
+  };
   const renderCoordinators = () => {
     return events?.event?.contact?.coordinators.map((coordinator, index) => (
       <Link key={index} href={coordinator.contact}>
@@ -85,36 +91,45 @@ const page = ({ params }: { params: { slug: string } }) => {
 
   const renderRules = () => {
     return events?.event?.rulesAndRegulations?.map((rule, index) => (
-      <Link key={index} href={`#${rule.topic}`} className={`${space.className} hover:underline font-bold text-xs leading-4`}>
+      <Link
+        key={index}
+        href={`#${rule.topic}`}
+        className={`${space.className} hover:underline font-bold text-xs leading-4`}
+      >
         RULES & REGULATIONS
       </Link>
     ));
   };
-const renderRulesAndRegulations = () => {
-  return events?.event?.rulesAndRegulations.map((rule, index) => (
-    <div key={rule._id} className="w-full h-auto flex flex-col gap-4">
-      <h1 id={`#${rule.topic}`}>{rule.topic}</h1>
-      <ul className={`list-disc ${space.className} font-normal text-base leading-5 tracking-[22%] text-[#FEFEFE]`}>
-        {rule.points.map((point, idx) => (
-          <li key={idx}>{point}</li>
-        ))}
-      </ul>
-    </div>
-  ));
-};
-const formatDate = (dateString:string) => {
-  if (!dateString) return "";
-  const date = new Date(dateString);
-  if (isNaN(date.getTime())) return "";
-  return date.toISOString().split('T')[0];
-};
-
+  const renderRulesAndRegulations = () => {
+    return events?.event?.rulesAndRegulations.map((rule, index) => (
+      <div key={rule._id} className="w-full h-auto flex flex-col gap-4">
+        <h1 id={`#${rule.topic}`}>{rule.topic}</h1>
+        {Array.isArray(rule.points) ? (
+          <ul
+            className={`list-disc ${space.className} font-normal text-base leading-5 tracking-[22%] text-[#FEFEFE]`}
+          >
+            {rule.points.map((point, idx) => (
+              <li key={idx}>{point}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>Invalid rule points</p>
+        )}
+      </div>
+    ));
+  };
+  const formatDate = (dateString: string) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    if (isNaN(date.getTime())) return "";
+    return date.toISOString().split("T")[0];
+  };
 
   return (
     <div className="h-screen w-screen overflow-x-hidden">
       <div className="size-full mt-20">
         <div className="h-[70%] w-screen flex items-center">
-          <div className="h-full w-[66%] flex flex-col gap-4 justify-center pl-20">
+          <div className="h-full w-[66%] flex flex-col gap-4 justify-center pl-5 sm:pl-20">
             <div className="h-10 w-full">
               <div className="flex gap-2 w-40">
                 <Link href="/competitions" className="flex gap-2">
@@ -135,34 +150,36 @@ const formatDate = (dateString:string) => {
                   {events?.event?.eventName.split("(")[1].slice(0, -1)}
                 </h1>
                 <h1
-                  className={`${panchang.className} font-semibold text-6xl leading-[4rem]`}
+                  className={`${panchang.className} font-semibold text-2xl sm:text-4xl md:text-5xl lg:text-6xl leading-[4rem]`}
                 >
                   {events?.event?.eventName?.split("(")[0]}
                 </h1>
               </div>
               <div className="w-auto h-8 flex items-center gap-2">
                 <div className="h-full w-32 rounded-3xl border border-[#555555]">
-                <RWebShare
-                  data={{
-                    text: "Share this event to your friends",
-                    url: `https://gecspectrum.com/competitions/${params.slug}`,
-                    title: "Spectrum",
+                  <RWebShare
+                    data={{
+                      text: "Share this event to your friends",
+                      url: `https://gecspectrum.com/competitions/${params.slug}`,
+                      title: "Spectrum",
                     }}
-                  onClick={() => console.log("shared successfully!")}
-                >
-                  <button
-                    className={`size-full px-1 py-2 flex gap-1 items-center justify-center ${space.className}`}
+                    onClick={() => console.log("shared successfully!")}
                   >
-                    <RiLinkM className="size-4 link" />{" "}
-                    <span className=" font-medium text-xs leading-4">
-                      SHARE EVENT
-                    </span>
-                  </button>
-                </RWebShare>  
+                    <button
+                      className={`size-full px-1 py-2 flex gap-1 items-center justify-center ${space.className}`}
+                    >
+                      <RiLinkM className="size-4 link" />{" "}
+                      <span className=" font-medium text-xs leading-4">
+                        SHARE EVENT
+                      </span>
+                    </button>
+                  </RWebShare>
                 </div>
                 <div className="h-full w-32 rounded-3xl border border-[#555555]">
                   <Link
-                    href={'https://www.instagram.com/gec.students.council24?igsh=MTF5c2wxd2pvY3pmNQ=='}
+                    href={
+                      "https://www.instagram.com/gec.students.council24?igsh=MTF5c2wxd2pvY3pmNQ=="
+                    }
                     className={`size-full px-1 py-2 flex gap-2 items-center justify-center ${space.className}`}
                   >
                     <RiInstagramFill className="size-4" />{" "}
@@ -220,7 +237,7 @@ const formatDate = (dateString:string) => {
                   >
                     1st ~{" "}
                     <span className="font-semibold text-tangerine">
-                    &#8377; {events?.event?.prices?.firstPlace}
+                      &#8377; {events?.event?.prices?.firstPlace}
                     </span>
                   </h1>
                   <h1
@@ -228,7 +245,7 @@ const formatDate = (dateString:string) => {
                   >
                     2nd ~{" "}
                     <span className="font-semibold text-tangerine">
-                    &#8377; {events?.event?.prices?.secondPlace}
+                      &#8377; {events?.event?.prices?.secondPlace}
                     </span>
                   </h1>
                 </div>
@@ -265,7 +282,10 @@ const formatDate = (dateString:string) => {
               </div>
               <div className={`mr-7 ${space.className} text-[#FFBA25]`}>
                 <h2 className=" font-bold leading-10 text-3xl">
-                  {events?.event?.teamSize} (TEAM)
+                  Max:{events?.event?.teamSize?.max} (TEAM)
+                </h2>
+                <h2 className=" font-bold leading-10 text-3xl">
+                  Min:{events?.event?.teamSize?.min} (TEAM)
                 </h2>
               </div>
             </div>
@@ -295,22 +315,22 @@ const formatDate = (dateString:string) => {
             <div className="h-[70%] w-full flex flex-col gap-4 text-[#FFBA25]">
               {renderRules()}
               <div className="w-full h-7 rounded-3xl text-white border border-[#555555]">
-              <RWebShare
+                <RWebShare
                   data={{
                     text: "Share this event to your friends",
                     url: `https://gecspectrum.com/competitions/${params.slug}`,
                     title: "Spectrum",
-                    }}
+                  }}
                   onClick={() => console.log("shared successfully!")}
                 >
-                <button
-                  className={`size-full px-1 py-2 flex gap-2 items-center justify-center ${space.className}`}
-                >
-                  <RiLinkM className="size-4 link" />{" "}
-                  <span className=" font-medium text-xs leading-4">
-                    SHARE EVENT
-                  </span>
-                </button>
+                  <button
+                    className={`size-full px-1 py-2 flex gap-2 items-center justify-center ${space.className}`}
+                  >
+                    <RiLinkM className="size-4 link" />{" "}
+                    <span className=" font-medium text-xs leading-4">
+                      SHARE EVENT
+                    </span>
+                  </button>
                 </RWebShare>
               </div>
             </div>
