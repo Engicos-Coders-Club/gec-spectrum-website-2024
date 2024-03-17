557492ce1d249e25c4c9782f7fcc1814b2b74e24
Introvert2000 <alvareschamp200@gmail.com>
2024-03-17 19:06:43 +0530

    added loader,check for overlapping bte hamburger n event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import ExploreTile from "@/components/explore-tile/ExploreTile";
 import Event from "./components/Event";
+import Loading from "./loading";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="overflow-x-hidden">
+     <Suspense fallback = {<Loading/>}>
       <Hero />
       <Tagline />
       <Event />
@@ -17,6 +20,7 @@ export default function Home() {
       {/* <Sponsors /> */}
       <ExploreTile />
       <Footer />
+     </Suspense>
     </main>
   );
 }
