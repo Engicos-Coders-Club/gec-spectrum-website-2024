873dcddbafc9dd571f78c875b6c5c4f6bc5dc0cd
Medam Greeshma <70336930+Greeshma2903@users.noreply.github.com>
2024-03-17 22:02:46 +0530

    UI fixes into production (#57)
    UI fixes
    
    ---------
    
    Co-authored-by: Akash Naik <veisthebest3@gmail.com>
    Co-authored-by: Medam Greeshma <70336930+Greeshma2903@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: shrey <shreysingh152004@gmail.com>
    Co-authored-by: Shridhar Kamat <52820662+Shridhar-dev@users.noreply.github.com>
    Co-authored-by: Rushikesh <141911720+Rushikesh-24@users.noreply.github.com>
    Co-authored-by: Joel Alvares <97362062+Introvert2000@users.noreply.github.com>
    Co-authored-by: Akash Naik <veisthebest3@gmail.com>

---
+++ src/app/loading.tsx
@@ -0,0 +1,15 @@
+import Animation from "./components/Events/animation"
+  const Loading = () => {
+
+    return(
+        <section>
+
+        <div>
+            <Animation/>
+        </div>
+
+        </section>
+    )
+  }
+
+  export default Loading
--- src/components/explore-tile/ExploreTile.tsx
+++ src/components/explore-tile/ExploreTile.tsx
@@ -1,28 +1,33 @@
 // ExploreTile.tsx
-import Tile from './tile/Tile';
-import localFont from 'next/font/local'
+import Tile from "./tile/Tile";
+import Link from "next/link";
+import localFont from "next/font/local";
 
 const panchang = localFont({
-  src: '../../../public/Panchang-Variable.ttf',
-  display: 'swap',
-})
+  src: "../../../public/Panchang-Variable.ttf",
+  display: "swap",
+});
 
 interface ExploreTileProps {}
 
 const ExploreTile: React.FC<ExploreTileProps> = () => {
   return (
     <div className="px-20 relative">
-      <div className='flex flex-row  items-center space-y-0 space-x-9'>
-        <div className='relative'>
-          <h1 className={`text-lg md:text-2xl lg:text-5xl text-white font-semibold uppercase ${panchang.className}`}>Explore</h1>
+      <div className="flex flex-row  items-center space-y-0 space-x-9">
+        <div className="relative">
+          <h1
+            className={`text-lg md:text-2xl lg:text-5xl text-white font-semibold uppercase ${panchang.className}`}
+          >
+            Explore
+          </h1>
         </div>
-        <div className='absolute -left-10 h-[1px] w-screen gradient-border -z-10'></div>
+        <div className="absolute -left-10 h-[1px] w-screen gradient-border -z-10"></div>
       </div>
 
-      <div className='flex flex-col sm:flex-row gap-4 mt-10'>
-        <Tile text='* FAQS' subStr='Got doubts?' />
-        <Tile text='* team' subStr='meet the' />
-        <Tile text='* rooms' subStr='book' />
+      <div className="flex flex-col sm:flex-row gap-4 mt-10">
+        <Tile href="/competitions" text="* Events" subStr="All competitions" />
+        <Tile href="/teams" text="* team" subStr="meet the" />
+        <Tile href="/accommodation" text="* rooms" subStr="book" />
       </div>
     </div>
   );
