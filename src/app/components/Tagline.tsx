cc2602993f8870adeac5cd98914dd986c99c0684
Akash Naik <veisthebest3@gmail.com>
2024-03-21 00:23:43 +0530

    removed the images from tagline

--- src/app/components/Tagline.tsx
+++ src/app/components/Tagline.tsx
@@ -20,13 +20,15 @@ const Tagline = () => {
       >
         {/* laptop thing is here */}
         <Image
-          className="sm:h-80 hidden md:block h-64 w-[auto] rotate-[25deg]"
+          className="sm:h-80 hidden  h-64 w-[auto] rotate-[25deg]"
           height={200}
           width={200}
           src="https://s3-alpha-sig.figma.com/img/52d7/c247/011c2659015ffd1b09c19a9e1e679abc?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dyWtkTEtycdTQ4i6UdG1Snx5j2R6OFCq97aTsBkHw5zbm3pLpB4dObVE0gt4n2vjIThK-qWLKLJ-3sE4K3ZVyc2tlpAVNPACNIWw3AsF-ihv72O8rNwGk94ZzqpzFEkoKeeTJSEbCpivVZxuJrKPRQv9LQ4ovyD2mWcLPoFrF-bg0OnxZlXgv8NYZ7nw0SKClFiLzxCwOzEKi2Erm60EZ6HVti5t~t0Ti8Q7xaasvc2o30WKx4BoH4uB45J9in0xdUsLHuHqd07gYDVitvs9vsAUuc3DjAFoNbUXf5auIzjWzC8tSWqociPiOcDpOfcEH1KZA1JYLNdm4leasWa4zQ__"
           alt="computer"
         />
       </motion.div>
+
+      
       <motion.div
         initial={{ x: 300, y: 300, opacity: 0 }}
         whileInView={{ x: 0, y: -100, opacity: 1 }}
@@ -35,13 +37,16 @@ const Tagline = () => {
       >
         {/* robot thing is here */}
         <Image
-          className="h-[40rem] hidden md:block  rotate-[-26deg] w-[auto]"
+          className="h-[40rem] hidden rotate-[-26deg] w-[auto]"
           height={600}
           width={600}
           src="https://s3-alpha-sig.figma.com/img/9882/f4b1/44b540a3c892da5a247649855a71b660?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=V2cUcMh2LVCLfDiEkeJxjBoxu33P1aGAYnkeLD6h9s6i2WUFqSGyqkejeu2-~yFJ3dwN8vtSeO~C07qlPSejjhfQoLsU15aZYOJezUii~M3rmW2boipfpS9N1Be6q8YzloKEhppCOnJj9umHVPYYrG3LaCbUcByRr0rsFdt5~Vq6XTey~0eCLS6w7A3Bg1uzi51tQDrHI5cfd18uPRnWVU7GJtzyCZHGURBrl5ashhXPVIi3KDVOv8SI--FBklnBuoVShGstBg7hXXpXeyBiMVZdXzw2YUpX5NRnC-aXRiD~Lmm-i93M5iPnbm5gfBS-sQg9PcbejbDJqJy6HWTEJw__"
           alt="robot"
         />
       </motion.div>
+
+
+
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
