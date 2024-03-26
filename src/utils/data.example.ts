59d67610102d54c6a06b478b914da73342b5db17
Shridhar Kamat <52820662+Shridhar-dev@users.noreply.github.com>
2024-03-27 00:11:25 +0530

    Revert "Website team data added"

--- src/utils/data.example.ts
+++ src/utils/data.example.ts
@@ -1,377 +1,128 @@
 export const EVENT = [
-  {
-    id: 1,
-    title: "ROBO SOCCER",
-    subtitle: "ROBO SOCCER",
-    date: "3 MARCH 2023",
-    type: "Team",
-    team_size: "3 - 5",
-    fee: "300",
-    contacts: [
-      {
-        name: "Harsh Kumar",
-        contact: "/",
-      },
-      {
-        name: "Shriteen Damaskar",
-        contact: "/",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "TECHNOTHON",
-    subtitle: "HACKATHON",
-    date: "5 MARCH 2023",
-    type: "Team",
-    team_size: "3 - 4",
-    fee: "300",
-    contacts: [
-      {
-        name: "Harsh Kumar",
-        contact: "/",
-      },
-      {
-        name: "Shriteen Damaskar",
-        contact: "/",
-      },
-    ],
-  },
+    {
+        id: 1,
+        title: "ROBO SOCCER",
+        subtitle: "ROBO SOCCER",
+        date: "3 MARCH 2023",
+        type: "Team",
+        team_size: "3 - 5",
+        fee:"300",
+        contacts: [
+            {
+                name: "Harsh Kumar",
+                contact: "/"
+            },
+            {
+                name: "Shriteen Damaskar",
+                contact: "/"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "TECHNOTHON",
+        subtitle: "HACKATHON",
+        date: "5 MARCH 2023",
+        type: "Team",
+        team_size: "3 - 4",
+        fee:"300",
+        contacts: [
+            {
+                name: "Harsh Kumar",
+                contact: "/"
+            },
+            {
+                name: "Shriteen Damaskar",
+                contact: "/"
+            }
+        ]
+    }
 ];
 
 export const EVENT_INFO = {
-  robo_soccer: [
-    {
-      id: 1,
-      title: "ROBO SOCCER",
-      subtitle: "ROBO SOCCER",
-      description:
-        "Pen is mightier than sword, but we geeks prefer to fight using code. Enter the arena to battle it out. Magic of your logic is what we care about.",
-      prizes: [
+    robo_soccer: [
         {
-          rank: 1,
-          cash: 4000,
-        },
+            id: 1,
+            title: "ROBO SOCCER",
+            subtitle: "ROBO SOCCER",
+            description: "Pen is mightier than sword, but we geeks prefer to fight using code. Enter the arena to battle it out. Magic of your logic is what we care about.",
+            prizes: [
+                {
+                    rank: 1,
+                    cash: 4000
+                },
+                {
+                    rank: 2,
+                    cash: 3000
+                }
+            ],
+            date: "3 MARCH 2023",
+            type: "Team",
+            team_size: "3 - 5",
+            contacts: [
+                {
+                    name: "Harsh Kumar",
+                    contact: "/"
+                },
+                {
+                    name: "Shriteen Damaskar",
+                    contact: "/"
+                }
+            ]
+        }
+    ],
+    technathon: [
         {
-          rank: 2,
-          cash: 3000,
-        },
-      ],
-      date: "3 MARCH 2023",
-      type: "Team",
-      team_size: "3 - 5",
-      contacts: [
-        {
-          name: "Harsh Kumar",
-          contact: "/",
-        },
-        {
-          name: "Shriteen Damaskar",
-          contact: "/",
-        },
-      ],
-    },
-  ],
-  technathon: [
-    {
-      id: 2,
-      title: "TECHNOTHON",
-      subtitle: "HACKATHON",
-      description:
-        "Pen is mightier than sword, but we geeks prefer to fight using code. Enter the arena to battle it out. Magic of your logic is what we care about.",
-      prizes: [
-        {
-          rank: 1,
-          cash: 4000,
-        },
-        {
-          rank: 2,
-          cash: 3000,
-        },
-      ],
-      date: "5 MARCH 2023",
-      type: "Team",
-      team_size: "3 - 4",
-      contacts: [
-        {
-          name: "Harsh Kumar",
-          contact: "/",
-        },
-        {
-          name: "Shriteen Damaskar",
-          contact: "/",
-        },
-      ],
-      rules: [
-        {
-          name: "RULES & REGULATIONS",
-          link: "/",
-          data: "",
-        },
-        {
-          name: "ARENA",
-          link: "/",
-          data: "",
-        },
-        {
-          name: "ROBOT SPECS",
-          link: "/",
-          data: "",
-        },
-        {
-          name: "GAMEPLAY",
-          link: "/",
-          data: "",
-        },
-      ],
-    },
-  ],
-};
+            id: 2,
+            title: "TECHNOTHON",
+            subtitle: "HACKATHON",
+            description: "Pen is mightier than sword, but we geeks prefer to fight using code. Enter the arena to battle it out. Magic of your logic is what we care about.",
+            prizes: [
+                {
+                    rank: 1,
+                    cash: 4000
+                },
+                {
+                    rank: 2,
+                    cash: 3000
+                }
+            ],
+            date: "5 MARCH 2023",
+            type: "Team",
+            team_size: "3 - 4",
+            contacts: [
+                {
+                    name: "Harsh Kumar",
+                    contact: "/"
+                },
+                {
+                    name: "Shriteen Damaskar",
+                    contact: "/"
+                }
+            ],
+            rules: [
+                {
+                    name: 'RULES & REGULATIONS',
+                    link: "/",
+                    data: '',
 
-export const Teamdata = [
-  {
-    title: "COUNCIL",
-    subTitle: "",
-    people: [
-      { name: "C Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "C Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "C Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "C Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "C Name", designation: "Designation", image: "TeamImage.jpeg" },
-    ],
-  },
-  {
-    title: "FACULTY",
-    subTitle: "ADVISORS",
-    people: [
-      { name: "F Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "F Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "F Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "F Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "F Name", designation: "Designation", image: "TeamImage.jpeg" },
-    ],
-  },
-  {
-    title: "COMPUTER",
-    subTitle: "CURSOR",
-    people: [
-      {
-        name: "Sarthak Bandodkar",
-        designation: "Chairman",
-        image: "TeamImage.jpeg",
-        links: {
-          social: "",
-          github: "",
-        },
-      },
-      {
-        name: "Shivam Raikar",
-        designation: "Magazine Secretary",
-        image: "TeamImage.jpeg",
-        links: {
-          social: "www.linkedin.com/in/shivamraikar",
-          github: "https://github.com/Proshivam23",
-        },
-      },
-    ],
-  },
-  {
-    title: "IT",
-    subTitle: "INTESA",
-    people: [
-      { name: "I Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "I Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "I Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "I Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "I Name", designation: "Designation", image: "TeamImage.jpeg" },
-    ],
-  },
-  {
-    title: "MECHANICAL",
-    subTitle: "MESA",
-    people: [
-        {
-            name: "Saiel Pai Kane",
-            designation: "General Secretary",
-            image: "TeamImage.jpeg",
-            links: {
-              social: "https://www.linkedin.com/in/saiel-p-6224451bb",
-              github: "",
-            },
-          },
-      { name: "M Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "M Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "M Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "M Name", designation: "Designation", image: "TeamImage.jpeg" },
-    ],
-  },
-  {
-    title: "ETC",
-    subTitle: "ASSESTS",
-    people: [
-      { name: "E Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "E Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "E Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "E Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "E Name", designation: "Designation", image: "TeamImage.jpeg" },
-    ],
-  },
-  {
-    title: "ENE",
-    subTitle: "ENEXA",
-    people: [
-        {
-            name: "Leah Fernandes",
-            designation: "General Secretary",
-            image: "TeamImage.jpeg",
-            links: {
-              social: "https://www.instagram.com/leah.salonee?igsh=cGRpMzE3b2Q5dDI2",
-              github: "",
-            },
-          },
-      { name: "EN Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "EN Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "EN Name", designation: "Designation", image: "TeamImage.jpeg" },
-      { name: "EN Name", designation: "Designation", image: "TeamImage.jpeg" },
-    ],
-  },
-  {
-    title: "CIVIL",
-    subTitle: "ACES",
-    people: [
-      {
-        name: "Omkar Shirodkar",
-        designation: "Chairman",
-        image: "TeamImage.jpeg",
-        links: {
-          social: "https://www.instagram.com/_shirodkaromkar16_?igsh=Zndtd3F0dnNkM3d6",
-          github: "",
-        },
-      },
-      {
-        name: "ANISHA LAXIMAN NAIK SHIRODKAR",
-        designation: "Placement Secretary of ACES",
-        image: "TeamImage.jpeg",
-        links: {
-          social: "https://www.instagram.com/___anisha17___?igsh=ZTcxZ3I3OGFjNTFk",
-          github: "",
-        },
-      },
-      {
-        name: "Swayta Belgaonkar",
-        designation: "Vice Chairman",
-        image: "TeamImage.jpeg",
-        links: {
-          social: "",
-          github: "",
-        },
-      },
-      {
-        name: "Meghasyam Gurudas Dessai",
-        designation: "Co-Auditor",
-        image: "TeamImage.jpeg",
-        links: {
-          social: "",
-          github: "",
-        },
-      },
-    ],
-  },
-  {
-    title: "WEBSITE",
-    subTitle: "CLUB",
-    people: [
-      {
-        name: "M. Greeshma",
-        designation: "Project Manager & UI Designer",
-        image: "/teamAssets/IMG-20230413-WA0038 - Medam Greeshma.jpg",
-        links: {
-          social: "https://linkedin.com/in/m-greeshma",
-          github: "https://github.com/greeshma290303",
-        },
-      },
-      {
-        name: "Sanjay S Kumar",
-        designation: "Project Manager",
-        image: "/teamAssets/IMG-20231221-WA0052~2 - Sanjay .S Kumar.jpg",
-        links: {
-          social: "https://www.instagram.com/sxnjayy",
-          github: "https://github.com/Quantaindew",
-        },
-      },
-      {
-        name: "Shridhar Kamat",
-        designation: "Project Manager",
-        image: "/teamAssets/1708882498734 (1) - Shridhar Kamat.jpeg",
-        links: {
-          social: "https://www.linkedin.com/in/shridhar-kamat-1015a41bb/",
-          github: "https://github.com/Shridhar-dev",
-        },
-      },
-      {
-        name: "Shrey Kumar Singh",
-        designation: "Project Manager",
-        image:
-          "/teamAssets/shrey - shrey singh.jpeg",
-        links: {
-          social: "https://www.instagram.com/sshreysingh/?hl=en",
-          github: "https://github.com/sshrey15",
-        },
-      },
-      {
-        name: "Akash Naik",
-        designation: "Frontend Developer",
-        image: "/teamAssets/Profile picture - Akash Naik.jpeg",
-        links: {
-          social: "https://www.linkedin.com/in/akashpnaik/",
-          github: "https://github.com/cakesandcarrots",
-        },
-      },
-      {
-        name: "Dhruv Rajeev Naik",
-        designation: "Frontend Developer",
-        image: "/teamAssets/IMG_2971 - Dhruv Naik.jpeg",
-        links: {
-          social: "",
-          github: "https://www.github.com/Droovian",
-        },
-      },
-      {
-        name: "Edgar Camelo",
-        designation: "Backend Developer",
-        image: "/teamAssets/20240105_212715-01 - Edgar Camelo.jpeg",
-        links: {
-          social: "",
-          github: "github.com/edgarAndrew",
-        },
-      },
-      {
-        name: "Harsh Telang",
-        designation: "Frontend Developer",
-        image: "/teamAssets/IMG_20230401_225049_506 - Harsh Telang.jpg",
-        links: {
-          social: "https://linkedin.com/in/harsh-telang-226a70265",
-          github: "https://github.com/Harsh-Telang",
-        },
-      },
-      {
-        name: "Kedron Lucas",
-        designation: "UI/UX Designer",
-        image: "/teamAssets/3ba0420c-a704-4f94-861d-c839351515d7 - 031 Kedron Lucas.jpg",
-        links: {
-          social: "https://www.instagram.com/this_is_ked/",
-          github: "https://github.com/k3Dron",
-        },
-      },
-      {
-        name: "Rushikesh Gaonkar",
-        designation: "Frontend Developer",
-        image: "/teamAssets/profilepicture - Rushikesh Gaonkar.jpeg",
-        links: {
-          social: "https://www.instagram.com/rushikesh24_05",
-          github: "https://github.com/Rushikesh-24",
-        },
-      },
-    ],
-  },
-];
+                },
+                {
+                    name: 'ARENA',
+                    link: "/",
+                    data: '',
+                },
+                {
+                    name: 'ROBOT SPECS',
+                    link: "/",
+                    data: '',
+                },
+                {
+                    name: 'GAMEPLAY',
+                    link: "/",
+                    data: '',
+                }
+            ]
+        }
+    ]
+}
