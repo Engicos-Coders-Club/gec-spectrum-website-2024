8d8896b941232ee6151c81f3a8dde28a8c02333c
Akash Naik <veisthebest3@gmail.com>
2024-03-27 02:08:30 +0530

    updates in astro club event

--- src/app/event/starparty/page.tsx
+++ src/app/event/starparty/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import localFont from "next/font/local";
 import PageHead from "@/app/components/PageHead";
 import { RiLink } from "react-icons/ri";
@@ -7,7 +7,7 @@ import { FaInstagram } from "react-icons/fa";
 export default function starparty() {
   return (
     <main>
-      <PageHead body="CAMP OUT" faltutext="(*)" title="STAR PARTY" />
+      <PageHead  body="CAMP OUT" faltutext="(*)" title="STAR PARTY" />
       <section className="flex gap-4 items-start flex-wrap md:flex-nowrap mx-5 xl:max-w-screen-xl xl:mx-auto my-5">
         {/* --------------------------  event details box -------------------------- */}
         <div className="px-6 py-4 bg-white/10  my-4 flex flex-col gap-5 w-full md:w-fit">
@@ -24,11 +24,22 @@ export default function starparty() {
             </p>
           </div>
           <div className="flex font-bold flex-col gap-1 justify-start">
-            <p className="text-sm">Registor</p>
+            <p className="text-sm">Brochure</p>
+            <a
+              className=" bg-violet-900 p-2 px-5 rounded-full rounded-tl-none font-semibold hover:bg-violet-500 uppercase text-center"
+              href="/Star_party_brochure.pdf"
+              download={true}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Brochure
+            </a>
+
+            <p className=" pt-3 text-sm">Registor</p>
             <a
               title="Register Online"
               className="bg-primary p-2 px-5 rounded-full rounded-tl-none font-semibold hover:bg-violet-500 uppercase text-center"
-              href="https://forms.gle/vkMkS8TFW15k5zZz8"
+              href="https://forms.gle/FhsKovm9tgtjBxdC8"
               target="_blank"
               rel="noreferrer"
             >
@@ -61,7 +72,7 @@ export default function starparty() {
                 className="border flex items-center gap-2 border-gray-500 text-sm py-1 px-4 rounded-full"
               >
                 <FaInstagram className="text-pink-500" />
-                FOLLOW US
+                <div className=" whitespace-nowrap">ASTRO CLUB</div>
               </Link>
             </div>
           </div>
@@ -69,97 +80,134 @@ export default function starparty() {
 
         <div className="container mx-auto px-4 text-gray-200 grow">
           {/* --------------------------  event faqs -------------------------- */}
-          <h2 className="text-lg md:text-2xl font-bold mt-6 mb-2 text-tangerine">
-            FAQs
+          <h2 className="text-lg md:text-2xl font-bold mt-6 mb-2 text-[#FA5622]">
+            What we got for you */
           </h2>
-          <p>
-            <strong>Accommodation Details:</strong>
-          </p>
-          <ul className="list-disc ml-6">
+
+          <ul className=" list-none">
             <li>
-              For boys, each room can accommodate up to 3 members (boys). For
-              girls, it&apos;s 1 or 2 members per room.
+              <strong className="text-[#FFBA25]">Night Sky observation️:</strong> spotting up and learning
+              about different constellation celestial bodies and satellite and
+              much more.
             </li>
+            <br />
             <li>
-              Accommodation will be available for 3 days (starting 2nd Apr).
+              <strong className=" text-[#FFBA25]">Campfire:</strong> Gather around the crackling fire as we
+              share stories, laughter, and warmth under the vast expanse of the
+              night sky. It's the perfect opportunity to connect with fellow
+              enthusiasts and share your passion for astronomy.{" "}
             </li>
-            <li>Boys and girls accommodation will be separate.</li>
+            <br />
             <li>
-              Attached washrooms are not available. Only common washrooms are
-              available.
+              <strong className="text-[#FFBA25]">Staying in Tents:</strong> Experience the thrill of
+              camping out under the stars as you spend the night in cozy tents.
+              Feel the magic of the night envelop you as you drift off to sleep
+              surrounded by the beauty of nature in tents after an adventurous
+              day.
+            </li>
+            <br />
+
+            <li>
+              {" "}
+              <strong className=" text-[#FFBA25]">Space Mythology Stories:</strong> Delve into the rich
+              tapestry of space mythology as we explore tales of gods, heroes,
+              and celestial phenomena. Discover the fascinating legends behind
+              the constellations that adorn the night sky and unlock the
+              mysteries of the cosmos.
+            </li>
+            <br />
+            <li>
+              {" "}
+              <strong className=" text-[#FFBA25]">GEC Campus Trail:</strong> Embark on a guided trail around
+              the GEC campus and discover hidden gems and scenic spots under the
+              moonlit sky. It's a chance to explore the beauty of our
+              surroundings and soak in the serenity of the night.
+            </li>
+            <br />
+            <li>
+              {" "}
+              <strong className="text-[#FFBA25]"> Astrophotography:</strong> capture up the pictures of
+              mesmerizing starry sky with the help of the camera and phone with
+              some expert advice.
+            </li>
+            <br />
+            <li className=" font-bold">
+              But that's not all! Our Star Party will also feature a special
+              treat: the Morning Planet Parade. Witness the breathtaking sight
+              of the planets as they align in the predawn sky, a rare spectacle
+              that you won't want to miss.
             </li>
           </ul>
-
-          <p className="mt-2">
-            <strong>Payment Details:</strong>
-          </p>
-          <ul className="list-disc ml-6">
-            <li>Total Accommodation Cost from 2nd to 5th April:</li>
-            <ul className="list-square ml-6">
-              <li>
-                <strong>Male:</strong> ₹500/person (Double/Triple Sharing) OR
-                ₹1000 (Individual).
-              </li>
-              <li>
-                <strong>Female:</strong> ₹500/person (Double Sharing) OR ₹1000
-                (Individual).
-              </li>
-            </ul>
-            <li>
-              <strong className="text-mango">Note:</strong> Payment will be
-              collected in person. A minimum caution deposit will be collected
-              at the time of payment.
-            </li>
-          </ul>
-
           {/* --------------------------  event tnc -------------------------- */}
           <h2 className="font-bold mt-6 mb-2 text-lg md:text-2xl text-tangerine">
-            T & C
+            RULEBOOK */
           </h2>
+          <h3 className="font-bold mt-6 mb-2 pl-2 text-lg md:text-xl text-[#FFBA25]">
+            {" "}
+            */ DO’S
+          </h3>
           <ul className="list-disc ml-6">
-            <li>Check in starts on 2nd April 2024, 3:00PM onwards.</li>
             <li>
-              Guests should carry a valid Gov ID proof and College ID (if
-              applicable) during the time of payment.
+              Carry jackets, caps, blankets, sleeping mats and necessary items
+              needed considering that it is an open air overnight event.
             </li>
             <li>
-              Proof of registration email should be provided during payment
-              (digital/physical).
+              Participants must stay within the demarcated area and must follow
+              all instructors of Coordinators at all costs.
+            </li>
+            <li>SHOES are compulsory.</li>
+            <li>
+              There will be no arrangement made for charging devices nor does
+              the management guarantee it. So participants must make sure that
+              their phones are charged/ should carry power banks if required.{" "}
             </li>
             <li>
-              Facilities included in payment: the room, mattress, and pillows.
+              CARRY Torchlights for the GEC trail and listen to all orders of
+              the coordinators. Participants are responsible for their
+              belongings.
+            </li>
+            <li>Participants should ensure their own transport from venue.</li>
+          </ul>
+
+          <h3 className="font-bold mt-6 mb-2 text-lg pl-2 md:text-xl text-[#FFBA25]">
+            */ DONT’S{" "}
+          </h3>
+          <ul className="list-disc ml-6">
+            <li>
+              Participants shouldn't be under any kind of influence like
+              ALCOHOL/ CIGARETTE.
             </li>
             <li>
-              Food is not included in the amount mentioned above. Guests can
-              purchase food from hostel mess/food facilities around campus.
-            </li>
-            <li>Guests are expected not to litter.</li>
-            <li>
-              Guests are expected to vacate the rooms by 5th April 2024, 10:00
-              AM.
-            </li>
-            <li>No refunds for full 3 days of accommodation.</li>
-            <li>
-              Refunds are only applicable in case Guest leaves earlier than 3
-              days (to be collected while vacating).
-            </li>
-            <li>
-              Caution deposit will be withheld if the rooms or beds are found to
-              be tampered on inspection.
+              Participants shouldn't be in possession of any
+              alcoholic/tobacco/narcotics substances.
             </li>
           </ul>
+          <p className=" pt-10 font-bold uppercase">
+            Note: Incase the organizers find anyone breaking the above mentioned
+            rules, a strict action will be taken against the person legally and
+            will be escorted by the security out of the venue!
+          </p>
 
           {/* --------------------------  event contact -------------------------- */}
           <h2 className="font-bold mt-6 mb-2 text-lg md:text-2xl text-tangerine">
-            Got Doubts? Contact
+            CONTACTS */{" "}
           </h2>
           <p>
-            Samarth Narvekar
+            Omkar Gavade (President):
             <a
-              href="https://api.whatsapp.com/send?phone=+919834941891"
+              href="https://api.whatsapp.com/send?phone=+918010969462"
               className="text-blue-500 hover:text-blue-200 mx-2"
             >
-              (+91 98349 41891)
+              (91 80109 69462)
+            </a>
+          </p>
+          <p>
+            Siya Pockle (Tech advisor):
+            <a
+              href="https://api.whatsapp.com/send?phone=+917057292485"
+              className="text-blue-500 hover:text-blue-200 mx-2"
+            >
+              (+91 70572 92485)
             </a>
           </p>
         </div>
