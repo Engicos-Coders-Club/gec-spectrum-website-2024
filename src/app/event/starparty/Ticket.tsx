ae89f8bf2676771e4c96a5b8034cd2c9e4ad57c0
Akash Naik <veisthebest3@gmail.com>
2024-03-27 02:54:15 +0530

    updates in astro club event

---
+++ src/app/event/starparty/Ticket.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Ticket = () => {
+  return (
+   <div className=' flex relative  '>
+    <img src="https://s3-alpha-sig.figma.com/img/ad1d/04bc/6f61a879d72ffb1b06bc92aedc25e232?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aWE7CONR9fVlcgPwv1ShbhXLTLlX25lW1QloljtylXQV-bSN1CKtWfL-Fq25pRG7WPkc8-aZSSe3fZhHSb0m0ajDbOKlRV0SvMN3B05aSodbCJH9igr3Q7l3RYeE1rkDj0y7ZUOmlhnx3msvtY3wbjNvFaI9r5v~AYfS3cBcmTpPU8GyycXydT1wpceJcJT8-Kpn1mUGGbgGPsLkXq9TiEQfKkwnLJ15Ggwp6OmeCqbUAa4LRtbIGFKawsmJ7GuiNcY2imNJ3FrHfzCjVjEjdQyoWyzvtXwfXKgLdp2ZMoK4i4M5r1QpAcj-pVYOCxv7pa3dO~ear86IzdSgMJchxw__" alt="" />
+    <img className=' absolute h-[9.2rem] w-[9.2rem] bottom-[9.15rem] left-[8rem]  ' src="https://s3-alpha-sig.figma.com/img/636d/41cb/4df186a956892c740c199327c6be90e2?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZoxFqrY11R2ZNLLuKC07V58RzNPuNwQ61I-N6egkpRdPzmh1mqJpgggFzYn4LJt33sFpbghEiiNPJ-spfFMqSfEjgLVFPS0m6N1NyoqvxJDTzFnRZT3w3OPkNraHd6Wfb3S3tYhVVNUgRmjRMXieM8PRYIjwjZh~H7VQCYpZgNqTXjV9RbLoOdOEZ9k39ix-XEZbiJkxaLEFmsdlDOxjmF~9zwyMZpoOPCLahroLdRxnHT6VrgcqAGybojSzuAyY3weqV604rgcDll7VL6ReEw2HWDUXU1XXruW750I1-5DUv2s8SvOAyUNwbfkGzo-hYci2aNV9v0NRh~U~KiBCuw__" alt="" />
+   </div>
+  )
+}
+
+export default Ticket
--- src/app/event/starparty/page.tsx
+++ src/app/event/starparty/page.tsx
@@ -1,134 +1,157 @@
 "use client";
-import localFont from "next/font/local";
 import PageHead from "@/app/components/PageHead";
 import { RiLink } from "react-icons/ri";
 import Link from "next/link";
 import { FaInstagram } from "react-icons/fa";
+import Image from "next/image";
+import Ticket from "./Ticket";
 export default function starparty() {
   return (
     <main>
-      <PageHead  body="CAMP OUT" faltutext="(*)" title="STAR PARTY" />
+      <PageHead body="CAMP OUT" faltutext="(*)" title="STAR PARTY" />
+      <div className="flex">
+        <Ticket></Ticket>
+      </div>
+
       <section className="flex gap-4 items-start flex-wrap md:flex-nowrap mx-5 xl:max-w-screen-xl xl:mx-auto my-5">
-        {/* --------------------------  event details box -------------------------- */}
-        <div className="px-6 py-4 bg-white/10  my-4 flex flex-col gap-5 w-full md:w-fit">
-          <div className="flex font-bold flex-col gap-1 justify-start">
-            <p className="text-sm">Date</p>{" "}
-            <p className="text-lg md:text-xl font-bold text-mango">
-              10PM - 3rd April, 2024{" "}
-            </p>
-          </div>
-          <div className="flex font-bold flex-col gap-1 justify-start">
-            <p className="text-sm">Venue</p>
-            <p className="text-lg md:text-xl font-bold text-mango">
-              Goa College of Engineering Campus{" "}
-            </p>
-          </div>
-          <div className="flex font-bold flex-col gap-1 justify-start">
-            <p className="text-sm">Brochure</p>
-            <a
-              className=" bg-violet-900 p-2 px-5 rounded-full rounded-tl-none font-semibold hover:bg-violet-500 uppercase text-center"
-              href="/Star_party_brochure.pdf"
-              download={true}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Brochure
-            </a>
+        <div>
+          <div className="px-6 py-4 bg-white/10  my-4 flex flex-col gap-5 w-full md:w-fit">
+            <div className="flex font-bold flex-col gap-1 justify-start">
+              <p className="text-sm">Date</p>{" "}
+              <p className="text-lg md:text-xl font-bold text-mango">
+                10PM - 3rd April, 2024{" "}
+              </p>
+            </div>
+            <div className="flex font-bold flex-col gap-1 justify-start">
+              <p className="text-sm">Venue</p>
+              <p className="text-lg md:text-xl font-bold text-mango">
+                Goa College of Engineering Campus{" "}
+              </p>
+            </div>
+            <div className="flex font-bold flex-col gap-1 justify-start">
+              <p className="text-sm">Brochure</p>
+              <a
+                className=" bg-violet-900 p-2 px-5 rounded-full rounded-tl-none font-semibold hover:bg-violet-500 uppercase text-center"
+                href="/Star_party_brochure.pdf"
+                download={true}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Brochure
+              </a>
 
-            <p className=" pt-3 text-sm">Registor</p>
-            <a
-              title="Register Online"
-              className="bg-primary p-2 px-5 rounded-full rounded-tl-none font-semibold hover:bg-violet-500 uppercase text-center"
-              href="https://forms.gle/FhsKovm9tgtjBxdC8"
-              target="_blank"
-              rel="noreferrer"
-            >
-              register
-            </a>
-            <div className="flex pt-4 items-center gap-3">
-              <button
-                onClick={() => {
-                  if (navigator.share) {
-                    navigator
-                      .share({
-                        title: document.title,
-                        text: "Star Party | GEC Spectrum 2024", // Replace with the static event name
-                        url: window.location.href,
-                      })
-                      .then(() => console.log("Successful share"))
-                      .catch((error) => console.log("Error sharing:", error));
+              <p className=" pt-3 text-sm">Registor</p>
+              <a
+                title="Register Online"
+                className="bg-primary p-2 px-5 rounded-full rounded-tl-none font-semibold hover:bg-violet-500 uppercase text-center"
+                href="https://forms.gle/FhsKovm9tgtjBxdC8"
+                target="_blank"
+                rel="noreferrer"
+              >
+                register
+              </a>
+              <div className="flex pt-4 items-center gap-3">
+                <button
+                  onClick={() => {
+                    if (navigator.share) {
+                      navigator
+                        .share({
+                          title: document.title,
+                          text: "Star Party | GEC Spectrum 2024", // Replace with the static event name
+                          url: window.location.href,
+                        })
+                        .then(() => console.log("Successful share"))
+                        .catch((error) => console.log("Error sharing:", error));
+                    }
+                  }}
+                  className="border flex items-center gap-2 border-gray-500 text-sm py-1 px-4 rounded-full"
+                >
+                  <RiLink className="text-blue-500" />
+                  <div className=" whitespace-nowrap">SHARE EVENT</div>
+                </button>
+
+                <Link
+                  href={
+                    "https://www.instagram.com/gecastronomyclub?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
                   }
-                }}
-                className="border flex items-center gap-2 border-gray-500 text-sm py-1 px-4 rounded-full"
-              >
-                <RiLink className="text-blue-500" />
-                <div className=" whitespace-nowrap">SHARE EVENT</div>
-              </button>
-
-              <Link
-                href={
-                  "https://www.instagram.com/gecastronomyclub?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-                }
-                className="border flex items-center gap-2 border-gray-500 text-sm py-1 px-4 rounded-full"
-              >
-                <FaInstagram className="text-pink-500" />
-                <div className=" whitespace-nowrap">ASTRO CLUB</div>
-              </Link>
+                  className="border flex items-center gap-2 border-gray-500 text-sm py-1 px-4 rounded-full"
+                >
+                  <FaInstagram className="text-pink-500" />
+                  <div className=" whitespace-nowrap">ASTRO CLUB</div>
+                </Link>
+              </div>
             </div>
           </div>
+          <Image
+            className=" h-[30rem] w-auto"
+            src="https://s3-alpha-sig.figma.com/img/f972/7036/2937d51126333d078052e33941f4ad86?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SCpUBCVyc1vC625up7GA78bzAGKaILn2tsuJ7CYY-lzQZPa3V4~0THkGG5K4rD3rWr55qAtjxFVYfj7iYJzA7p-d1KRX~KNAg3FLPSR73COLKBy~w1J4ebzCnG2TF7caCKArmEqIW39aQZAdZ0JZgMyZC9fKFG7cx4Z~EZWfzNyNPk3BKHPEF1lW1DkK5FDsdNgnZlNTHHorQ4XKk3s6do4mP7eMnbJ8L~uJMIQxPJsB3OAAuDQfQKRAYTZGyuLPvaQCGNy75GBZOTFLYlIQTL~Rp7-tYNbX6Nx8A9sJSEL6auJcUKbW21M3sOGf0VvSBkYg2hSDH9RlO-05F1vmDw__"
+            height={200}
+            width={200}
+            alt="What is love?"
+          ></Image>
         </div>
 
         <div className="container mx-auto px-4 text-gray-200 grow">
-          {/* --------------------------  event faqs -------------------------- */}
           <h2 className="text-lg md:text-2xl font-bold mt-6 mb-2 text-[#FA5622]">
             What we got for you */
           </h2>
 
           <ul className=" list-none">
             <li>
-              <strong className="text-[#FFBA25]">Night Sky observation️:</strong> spotting up and learning
-              about different constellation celestial bodies and satellite and
-              much more.
+              <strong className="text-[#FFBA25]">
+                Night Sky observation️:
+              </strong>{" "}
+              spotting up and learning about different constellation celestial
+              bodies and satellite and much more.
             </li>
             <br />
             <li>
-              <strong className=" text-[#FFBA25]">Campfire:</strong> Gather around the crackling fire as we
-              share stories, laughter, and warmth under the vast expanse of the
-              night sky. It's the perfect opportunity to connect with fellow
-              enthusiasts and share your passion for astronomy.{" "}
+              <strong className=" text-[#FFBA25]">Campfire:</strong> Gather
+              around the crackling fire as we share stories, laughter, and
+              warmth under the vast expanse of the night sky. It's the perfect
+              opportunity to connect with fellow enthusiasts and share your
+              passion for astronomy.{" "}
             </li>
             <br />
             <li>
-              <strong className="text-[#FFBA25]">Staying in Tents:</strong> Experience the thrill of
-              camping out under the stars as you spend the night in cozy tents.
-              Feel the magic of the night envelop you as you drift off to sleep
-              surrounded by the beauty of nature in tents after an adventurous
-              day.
+              <strong className="text-[#FFBA25]">Staying in Tents:</strong>{" "}
+              Experience the thrill of camping out under the stars as you spend
+              the night in cozy tents. Feel the magic of the night envelop you
+              as you drift off to sleep surrounded by the beauty of nature in
+              tents after an adventurous day.
             </li>
             <br />
 
             <li>
               {" "}
-              <strong className=" text-[#FFBA25]">Space Mythology Stories:</strong> Delve into the rich
-              tapestry of space mythology as we explore tales of gods, heroes,
-              and celestial phenomena. Discover the fascinating legends behind
-              the constellations that adorn the night sky and unlock the
-              mysteries of the cosmos.
+              <strong className=" text-[#FFBA25]">
+                Space Mythology Stories:
+              </strong>{" "}
+              Delve into the rich tapestry of space mythology as we explore
+              tales of gods, heroes, and celestial phenomena. Discover the
+              fascinating legends behind the constellations that adorn the night
+              sky and unlock the mysteries of the cosmos.
             </li>
             <br />
             <li>
               {" "}
-              <strong className=" text-[#FFBA25]">GEC Campus Trail:</strong> Embark on a guided trail around
-              the GEC campus and discover hidden gems and scenic spots under the
-              moonlit sky. It's a chance to explore the beauty of our
-              surroundings and soak in the serenity of the night.
+              <strong className=" text-[#FFBA25]">
+                GEC Campus Trail:
+              </strong>{" "}
+              Embark on a guided trail around the GEC campus and discover hidden
+              gems and scenic spots under the moonlit sky. It's a chance to
+              explore the beauty of our surroundings and soak in the serenity of
+              the night.
             </li>
             <br />
             <li>
               {" "}
-              <strong className="text-[#FFBA25]"> Astrophotography:</strong> capture up the pictures of
-              mesmerizing starry sky with the help of the camera and phone with
-              some expert advice.
+              <strong className="text-[#FFBA25]">
+                {" "}
+                Astrophotography:
+              </strong>{" "}
+              capture up the pictures of mesmerizing starry sky with the help of
+              the camera and phone with some expert advice.
             </li>
             <br />
             <li className=" font-bold">
@@ -195,16 +218,16 @@ export default function starparty() {
           <p>
             Omkar Gavade (President):
             <a
-              href="https://api.whatsapp.com/send?phone=+918010969462"
+              href="https://api.whatsapp.com/send?phone=918010969462"
               className="text-blue-500 hover:text-blue-200 mx-2"
             >
-              (91 80109 69462)
+              (+91 80109 69462)
             </a>
           </p>
           <p>
             Siya Pockle (Tech advisor):
             <a
-              href="https://api.whatsapp.com/send?phone=+917057292485"
+              href="https://api.whatsapp.com/send?phone=917057292485"
               className="text-blue-500 hover:text-blue-200 mx-2"
             >
               (+91 70572 92485)
