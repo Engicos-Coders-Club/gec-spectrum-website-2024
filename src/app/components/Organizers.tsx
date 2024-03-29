5ccb612cd1db80f152e794aa14c427acf066d844
Greeshma Medam <70336930+Greeshma2903@users.noreply.github.com>
2024-03-29 12:22:45 +0530

    feat: Add title sponsors

--- src/app/components/Organizers.tsx
+++ src/app/components/Organizers.tsx
@@ -26,18 +26,19 @@ const organizers = () => {
         transition={{ duration: 1.2, delay: 0.1 }}
       >
         {/* Main Council */}
-        <div className=" bg-white p-4 rounded-[50%] h-28 w-28 md:h-32 md:w-32 object-contain overflow-hidden relative">
+        <div className=" bg-white p-4 rounded-[50%] h-24 w-24 md:h-28 md:w-28 object-contain overflow-hidden relative">
           <Image
-            className="rounded-full"
+            className="rounded-full h-full w-full p-2"
             src="https://s3-alpha-sig.figma.com/img/928b/a162/c80d9ca7de4645dd97637aa03c0ae9b6?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Cr5KoJDHC9aOsUP14Wnsal5wGeLe4ZH122XnEdLZ~ac~E93IuGIh~4OFM1MDoWXu80Y83rrPH4WlZbCYGf1WA59lI-cEVL9p~tUJ0BzB1hMsiUSy9KaOZPM0YURg5~C3SwyKyOeG7y53KvNVOL1GykfLPGs8IstTb1geW9vTLHbXbXVh6-cwI~3RPWEYT5k4dxOeDd6PuW6WruKJEsGRwc4HhiS9q3HOcxsdFVdsumqXnwXKeF9KZ7VBZ8IyAgQgbVne1kc3dX2lherJ2fONMwMxP7fFHIV0uBXo2EhXVwnFcRXCUq5n3RfI0v7UCifiubuu6h-JEPtuWqjnj1v-tw__"
             fill
+            sizes="(max-width: 640px) 50vw, (max-width: 768px) 50vw, 25vw"
             alt="Student Council, GEC"
             title="Student Council, GEC"
           />
         </div>
 
         {/* Computer department */}
-        <div className=" bg-white p-4 h-28 w-28 md:h-32 md:w-32 rounded-full object-contain overflow-hidden relative">
+        <div className=" bg-white p-4 h-24 w-24 md:h-28 md:w-28 rounded-full object-contain overflow-hidden relative">
           <Image
             className="p-5"
             src="https://s3-alpha-sig.figma.com/img/459f/b858/fa5b841ca545e6a203b866af868b0638?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WOVGxgDfii06i67OyloF3r3-I7fRH55BLlRmDXjh25w2dJEL~Bcf2rdFYH-w9vpRNTgmGSGSyDbXn-YI7c8jspx0-kgM~jvdqL-FQCo0s7GY1rE0cFe4CsW0i84iKk7AL7ftFv3790Gd0gXonAz~LBA4QSFwD29LXziMBKy4FQCxLP0I3OJff8VzpWZXjQwHjo48uyGhlcqHcAv-wD3dgrVFPEIYw473DCnrZLOCUjsfitm4N3OYwKKGZmZJ08iSzv-zouLz5ii0PDNXzzUtctc6TvhAKygPGzH-E~qQCCVkJ9oMKnxxnhUBMQR-AVXhDF-JXwSBsNNZ1iTg7g2Mrw__"
@@ -48,7 +49,7 @@ const organizers = () => {
           ></Image>
         </div>
         {/* ENE department */}
-        <div className="bg-black p-4 h-28 w-28 md:h-32 md:w-32 rounded-full object-contain overflow-hidden relative">
+        <div className="bg-black p-4 h-24 w-24 md:h-28 md:w-28 rounded-full object-contain overflow-hidden relative">
           <Image
             className=""
             src="https://s3-alpha-sig.figma.com/img/e8f7/c7c3/cdf7719c37faca8328b5ee97355c84b2?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HS-CQkScj2w81LOLNVyUaaOqfVP59Av4uQeAVfSCCKicJPU0OjRlDhk66AlidmyBaLbRO7sdga8uCDU~gu0EtD~JQb-cY270MXmbR0GcV3Kbnd3jnV~1eJ5cXveQQ0aANv3aIqtv650dT2NLvZl3fOBNUblfW5Mlqhvg6WPq2qMdb7mKtpe-B4QbwLTPGpT4x9vriziQPoqfGRVNnKgC8sDYG6KhwIrpV9ZsRMtAAfarmzElk8WnoFasRQGvRLuPXeyGt2lPS7JNIvIlWxXkWcC9HWiG~ym7UUr3hzsQFjTWNajgkdi9om3V5SDC5Uidpfn~wmNOb9S3fwnDIgT~Ww__"
@@ -59,7 +60,7 @@ const organizers = () => {
           ></Image>
         </div>
         {/* IT department */}
-        <div className="bg-white p-4 h-28 w-28 md:h-32 md:w-32 rounded-full object-contain overflow-hidden relative">
+        <div className="bg-white p-4 h-24 w-24 md:h-28 md:w-28 rounded-full object-contain overflow-hidden relative">
           <Image
             className=""
             src="https://s3-alpha-sig.figma.com/img/7d7f/8e40/e187d827e6f1b69f9dcaf34113ec70bb?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ClrC9GQPJlY17qwoSNQMmcbedwlWKU8zyCmiXE9aY7vO05YCHcWecIVRg9j0NB~fvQWV99OyOZ8~rC4B01aELssptRy45PmuBRpR8w~3g8ff8WCBdijY3ZBUvOYaypBNI61zQaewcfo-8cGZYJF1vecDAYnH4-BMoJlaWW6pbDpc7hwPK5499ckjfZ9nefLv6W6FGyYpVsct~RrymO7nGogh7k3NbFDK0JVbItbMPybwFCVDFpX4FfA42fVodCu4UH9M-Orpv5~H70Gam93xDdCACoJgr0rPntE5iekKWhHSTr0sIME74yhSzCCtihbNQljEyxaKUSxTsXAlvgLyhA__"
@@ -70,7 +71,7 @@ const organizers = () => {
           ></Image>
         </div>
         {/* Civil department */}
-        <div className="bg-white p-4 h-28 w-28 md:h-32 md:w-32 rounded-full object-contain overflow-hidden relative">
+        <div className="bg-white p-4 h-24 w-24 md:h-28 md:w-28 rounded-full object-contain overflow-hidden relative">
           <Image
             className="p-3"
             src="https://s3-alpha-sig.figma.com/img/c2e2/db7c/54434f622eaaf7725308918c3ce77a7a?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OQMXQ~zWUs7Qd6lwmfViUSflIDbXBwiXuRWOZGTWT62snWRGraHA~EgTbeVhfnzU~G4H4FlrTIfL75Goh~1qWa1r2fvuCbolBGiU72Ro3ch6KfGGH7EMzq6~yatcgqe9TN2UVrYzQv~FxDwz9OcgDTnXoHMn23fjpHGHVTgeslRnrQfl~CwT6k~H~XuFUicGh5Levwzb9E2S5u1~w9TiZPOMa2303u~JEp5BET93JtoNfUERyNcRAWlQ1CSPt6JHCosVQMdWGjDu0KnI~pbxvCkvadDwsVrPP7RIGaODC9RD8bk1aGcCd5pQohYaQckynJFSd0E5-CP0psqZTnhRGw__"
@@ -81,7 +82,7 @@ const organizers = () => {
           ></Image>
         </div>
 
-        <div className="bg-white p-4 h-28 w-28 md:h-32 md:w-32 rounded-full object-contain overflow-hidden relative">
+        <div className="bg-white p-4 h-24 w-24 md:h-28 md:w-28 rounded-full object-contain overflow-hidden relative">
           <Image
             className="p-2"
             src="https://s3-alpha-sig.figma.com/img/1a84/492d/e53b6c75a5c019df7825c9b95140ccde?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hUKIO~BwOza5hyAeEbhQLILpjk78htzybC3X7VQr9YW5Zs4AdSBOmRZpv1U6FrQf7HRX3rY-e~Yk4VfrzCEvpeNbIRxgj7eJE6yoNgKdK12lQnf-O1sFVW~qL2jgR9cz2DAfQlnPIVY0SGou42NvhfJJi8rCK0~U621u1wcY9CT9u~AKvCZIzwqrh7CJDWppKhqKsN7ifQXI~Cd0RTTZGMjY9xKwZRHlS2oHBBWak~qAX-q0TL5i56ICyFSnTXnVRZoNpfibV09WAKBATVUo7KqUvlzn6eGAxCT3iuLhpj7GSjQ9VsyyxjwkIVQ4h6L8Jw42MCidyAz0jNpBi~-rXw__"
@@ -93,7 +94,7 @@ const organizers = () => {
         </div>
 
         {/* ETC Department */}
-        <div className="bg-white p-4 h-28 w-28 md:h-32 md:w-32 rounded-full object-contain overflow-hidden relative">
+        <div className="bg-white p-4 h-24 w-24 md:h-28 md:w-28 rounded-full object-contain overflow-hidden relative">
           <Image
             className="p-3"
             src="https://s3-alpha-sig.figma.com/img/17cd/b99b/af99fad626276586f8b1c694567f7a4a?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=b0z6NWYBV8HomJQqgAE1NqYfA2wuVV0yHkZfHYpnW99C6fIvhW7H-lLXr~IwgOdXQAW05QxVppWzsVEbpE~5-gmbmRzR8SGWKucXurLSPIJIx8zlFBsj6l05wMsU6xgadShMqkW-kFfoL2lrw12bdxHmBtH3MXNpkH1fh9J3efgdDYfFrdhkunWtgQa7qH~PrNGc6RjPaLSUeG1hYBermZ5h6vTkoEZJ1QDLkAFZUQrJRpVLEqy7ui06Y5c10Io8V4I6H0A5bDqP2rvBi8xNov6w--b-UuW2tAK8QXoSy3O9v95x8qiVjhNEJd7ce29eFUsM~BlMNM1Uja82PZhFDg__"
---
+++ src/components/SponsorSection.tsx
@@ -0,0 +1,140 @@
+import chetak from "@/assets/sponsors/chetak-event.svg";
+import ktm from "@/assets/sponsors/KTM-event.jpg";
+import yanni from "@/assets/sponsors/yanni-plast.jpg";
+import husqvarna from "@/assets/sponsors/husqvarna-motorcycles-event.png";
+import vinsanworld from "@/assets/sponsors/vinsanworld.png";
+import dempo from "@/assets/sponsors/dempo.jpeg";
+import Image from "next/image";
+
+const SponsorSection = () => {
+  return (
+    <>
+      <div className="w-full flex items-center justify-center flex-col mt-10">
+        <h3 className="text-xl md:text-3xl mt-10 mb-6 uppercase font-semibold flex items-center gap-2">
+          <span className="bg-tangerine size-3 md:size-5 rounded-full block">
+            {" "}
+          </span>{" "}
+          Title SPONSORS
+        </h3>
+        <div className="md:w-3/5 flex gap-5 md:gap-10  flex-col md:flex-row flex-wrap justify-around items-center">
+          <a
+            title="Vinsan World"
+            href="https://vinsanworld.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`bg-[#fff] w-64 h-32 mx-auto md:w-96 lg:w-1/2 md:h-52 relative object-contain hover:scale-105 transition-transform duration-300 ease-in-out py-2 px-3 md:py-5 flex justify-center rounded-xl border-gray-400 border-4`}
+          >
+            <Image
+              src={vinsanworld}
+              height={vinsanworld.height}
+              width={vinsanworld.width}
+              alt="Vinsan World Logo"
+              className="object-contain p-3"
+            />
+          </a>
+          <a
+            title="The House of Dempo"
+            href="https://www.dempos.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`bg-[#fff] w-64 h-32 mx-auto md:w-96 lg:w-1/2 md:h-52 relative object-contain hover:scale-105 transition-transform duration-300 ease-in-out py-2 px-3 md:py-5 flex justify-center rounded-xl border-sky-200 border-4`}
+          >
+            <Image
+              src={dempo}
+              height={dempo.height}
+              width={dempo.width}
+              alt="The House of Dempo Logo"
+              className="object-contain"
+            />
+          </a>
+        </div>
+      </div>
+      <br />
+      {/* ------------ Co sponsors ------------- */}
+      <div className="w-full flex items-center justify-center flex-col mt-10">
+        <h3 className="text-xl md:text-3xl mt-10 mb-6 uppercase font-semibold flex items-center gap-2">
+          <span className="bg-mango size-3 md:size-5 rounded-full block">
+            {" "}
+          </span>{" "}
+          Co SPONSORS
+        </h3>
+        <div className="mx-auto w-full">
+          <a
+            title="Yanni Infra Projects"
+            href="https://www.yanniinfraprojects.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`bg-[#fff] w-52 mx-auto h-28 md:w-72 md:h-44 relative object-contain hover:scale-105 transition-transform duration-300 ease-in-out py-2 px-3 md:py-5 flex justify-center rounded-xl border-gray-400 border-4`}
+          >
+            <Image
+              src={yanni}
+              height={yanni.height}
+              width={yanni.width}
+              alt="Yanni Infra Projects"
+              className="object-contain"
+            />
+          </a>
+        </div>
+      </div>
+      <br />
+
+      {/* ------------ Event Sponsors ------------- */}
+      <div className="w-full h-full flex items-center justify-center flex-col bg-black">
+        <h3 className="text-xl md:text-3xl mt-10 mb-6 uppercase font-semibold flex items-center gap-2">
+          <span className="bg-primary size-3 md:size-5 rounded-full block">
+            {" "}
+          </span>{" "}
+          Event SPONSORS
+        </h3>
+        <div className="md:w-3/5 flex gap-5 md:gap-10  flex-col md:flex-row flex-wrap justify-around items-center">
+          <a
+            href="https://www.chetak.com/"
+            title="Bajaj Chetak Electric Scooters"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`bg-[#22d3ee] w-44 h-20 md:w-60 md:h-32 relative hover:scale-105 transition-transform duration-300 ease-in-out px-3 py-5 flex justify-center rounded-xl border-[#25a2b5] border-4`}
+          >
+            <Image
+              src={chetak}
+              height={chetak.height}
+              width={chetak.width}
+              alt="Bajaj Chetak Electric Scooters"
+              className="object-contain"
+            />
+          </a>
+          <a
+            title="KTM Race Motorcycles"
+            href="https://www.ktm.com/en-in.html/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`bg-[#FF6600] w-44 h-20 md:w-60 md:h-32 relative object-contain hover:scale-105 transition-transform duration-300 ease-in-out px-3  flex justify-center items rounded-xl border-[#9b572a] border-4`}
+          >
+            <Image
+              src={ktm}
+              height={ktm.height}
+              width={ktm.width}
+              alt="KTM Race Motorcycles"
+              className="object-contain"
+            />
+          </a>
+          <a
+            title="Husqvarna Motorcycles"
+            href="https://www.husqvarna-motorcycles.com/en-in.html/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`bg-[#002550] w-44 h-20 md:w-60 md:h-32 relative object-contain hover:scale-105 transition-transform duration-300 ease-in-out py-2 px-3 md:py-5 flex justify-center rounded-xl border-sky-800 border-4`}
+          >
+            <Image
+              src={husqvarna}
+              height={husqvarna.height}
+              width={husqvarna.width}
+              alt="Husqvarna Motorcycles"
+              className="object-contain"
+            />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+export default SponsorSection;
